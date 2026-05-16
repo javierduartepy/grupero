a5cf446d3143b30f1ad586c8281733f4a844a1c9
javierduarte <javierduarte@ecom.com.ar>
2026-05-16 04:51:20 -0300

    fix: actualizar main/routes.py para SQLAlchemy

--- app/main/routes.py
+++ app/main/routes.py
@@ -6,5 +6,5 @@ main_bp = Blueprint('main', __name__, template_folder='../templates/main')
 
 @main_bp.route('/')
 def index():
-    total_personas = len(Persona.cargar_todas())
+    total_personas = Persona.query.count()
     return render_template('main/index.html', total_personas=total_personas)
