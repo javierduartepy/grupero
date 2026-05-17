4640dd11f7f872cff8c606518731db2fd1d59f59
javierduarte <javierduarte@ecom.com.ar>
2026-05-16 23:19:50 -0300

    feat: add total groups count to main index page

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from app.models.persona import Persona
+from app.models.grupo import Grupo
 
 main_bp = Blueprint('main', __name__, template_folder='../templates/main')
 
@@ -7,4 +8,5 @@ main_bp = Blueprint('main', __name__, template_folder='../templates/main')
 @main_bp.route('/')
 def index():
     total_personas = Persona.query.count()
-    return render_template('main/index.html', total_personas=total_personas)
+    total_grupos = Grupo.query.count()
+    return render_template('main/index.html', total_personas=total_personas, total_grupos=total_grupos)
